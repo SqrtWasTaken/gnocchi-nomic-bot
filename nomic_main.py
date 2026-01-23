6061ae670caa9f4d3c90e3ee2cb64d5ae48ebbc2
SqrtWasTaken <lingfeistang1@gmail.com>
2026-01-23 18:18:38 -0500

    use reactionmenu without python modules

--- nomic_main.py
+++ nomic_main.py
@@ -5,6 +5,9 @@ import sqlite3
 from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
+from reactionmenu.views_menu import ViewMenu
+from reactionmenu.buttons import ViewButton
+
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -41,14 +44,14 @@ def last_space_index(text):
     else:
         return text.rfind(' ')
 
-async def send_long_embeds(title, msg, interaction):
-    if len(msg) > 4096:
-        next_msg = msg[0:last_space_index(msg[0:4096])]
+async def send_long_embeds(title, msg, interaction, max_length):
+    if len(msg) > max_length:
+        next_msg = msg[0:last_space_index(msg[0:max_length])]
         remaining = len(msg) - len(next_msg)
         await interaction.response.send_message(embed=discord.Embed(title=title, description=next_msg, color=0xf5c12f))
         while remaining > 0:
-            if remaining > 4096:
-                next_msg = msg[len(msg)-remaining:last_space_index(msg[0:len(msg)-remaining+4096])]
+            if remaining > max_length:
+                next_msg = msg[len(msg)-remaining:last_space_index(msg[0:len(msg)-remaining+max_length])]
                 remaining -= len(next_msg)
                 await interaction.channel.send(embed=discord.Embed(title=title, description=next_msg, color=0xf5c12f))
             else:
@@ -57,20 +60,39 @@ async def send_long_embeds(title, msg, interaction):
     else:
         await interaction.response.send_message(embed=discord.Embed(title=title, description=msg, color=0xf5c12f))
 
+async def send_menu(title, msg, interaction, max_length):
+    menu = ViewMenu(interaction, menu_type=ViewMenu.TypeEmbed)
+
+    remaining = len(msg)
+    while remaining > 0:
+        if remaining > max_length:
+            next_msg = msg[len(msg)-remaining:last_space_index(msg[0:len(msg)-remaining+max_length])]
+            remaining -= len(next_msg)
+            menu.add_page(discord.Embed(title=title, description=next_msg, color=0xf5c12f))
+        else:
+            menu.add_page(discord.Embed(title=title, description=msg[len(msg)-remaining:], color=0xf5c12f))
+            break
+    
+    menu.add_button(ViewButton.back())
+    menu.add_button(ViewButton.next())
+    
+    await menu.start()
 
 # ======COMMANDS======
 # help
 @bot.tree.command(name="help", description="View commands.")
 async def help(interaction: discord.Interaction):
     await interaction.response.send_message(embed=discord.Embed(title="Help", 
-                            description='''`/rule [number]` - Look up a rule.
+                            description='''`/rule [number] [max_length]` - Look up a rule. Enter 0 to send the whole rule across multiple messages, or a number between 1-4096 to send a single embed with pagination.
 `/find_text [text]` - Look up rules containing a string. Ignores all alphanumeric characters.''', 
 color=0xf5c12f))
 
 
 # rule lookup
 @bot.tree.command(name="rule", description="Look up a rule.")
-async def rule(interaction: discord.Interaction, number: int):
+@app_commands.describe(number='ordinal number of rule', 
+                       max_length='Max chars in embeds, up to 4096. Enter 0 to send the whole rule')
+async def rule(interaction: discord.Interaction, number: int, max_length: int=4096):
     conn = sqlite3.connect(data_file)
     cursor = conn.cursor()
 
@@ -85,7 +107,13 @@ async def rule(interaction: discord.Interaction, number: int):
         else:
             text = 'Immutable'
 
-        await send_long_embeds(text+' Rule ' + str(number), row[0], interaction)
+        if max_length == 0:
+            await send_long_embeds(text+' Rule ' + str(number), row[0], interaction, 4096)
+        else:
+            if max_length > 0 and max_length <= 4096:
+                await send_menu(text+' Rule ' + str(number), row[0], interaction, max_length)
+            else:
+                await interaction.response.send_message('Please enter a number between 0 and 4096', ephemeral=True)
 
     conn.close()
 
